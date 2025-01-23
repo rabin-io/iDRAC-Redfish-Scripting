aec30c15a1e5e4007c6372be7e87f788c75c8e34
Rabin Yasharzadehe <rabin@rabin.io>
2025-01-23 13:20:25 +0200

    refactor

--- Redfish Python/wipe-and-set-raid-config.sh
+++ Redfish Python/wipe-and-set-raid-config.sh
@@ -4,14 +4,72 @@ IDRAC_HOST=${1:?Missing host}
 IDRAC_USER=root
 IDRAC_PASS=calvin
 
+export SSHPASS=${IDRAC_PASS}
+
 # RAID controller common name, can be changed btween systems, but for now it's the same
 RAID_CONTROLLER=RAID.Slot.1-1
 PV_SUFFIX=Enclosure.Internal.0-1:RAID.Slot.1-1
 
+function racadm() {
+    local SSHPASS=${IDRAC_PASS:-calvin}
+    sshpass -e ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o PreferredAuthentications=keyboard-interactive,password $*
+}
+
+function get_pdisks_with_attributes() {
+	local host_name=${1:?Missing Hostname}
+
+	racadm.sh "${host_name}" storage get pdisks --refkey RAID.Slot.1-1 -o -p state,status,mediatype | awk '
+/^Disk/ {
+    if (disk != "") print disk, state, status, media
+    disk = $0
+    state = status = media = ""
+}
+/State/ { state = $3 }
+/Status/ { status = $3 }
+/MediaType/ { media = $3 }
+END { print disk, state, status, media }
+'
+#Disk.Bay.0:Enclosure.Internal.0-1:RAID.Slot.1-1 Online Ok SSD
+#Disk.Bay.1:Enclosure.Internal.0-1:RAID.Slot.1-1 Online Ok SSD
+#Disk.Bay.2:Enclosure.Internal.0-1:RAID.Slot.1-1 Online Ok HDD
+}
+
+function generate_pdisk_list_from_index() {
+    local indexs=($@)
+    local delimiter=","
+    local disks=()
+    for i in "${indexs[@]}"; do
+        disks+=("Disk.Bay.${i}:${PV_SUFFIX}")
+    done
+
+    # Join the array elements with the delimiter
+    joined_string=$(IFS="$delimiter"; echo "${disks[*]}")
+
+    # Print the result
+    echo "$joined_string"
+}
+
+filter_disks() {
+  local disk_type="$1"
+  local matching_lines
+
+  # filter lines and join them with a comma
+  matching_lines=$(awk '/'$disk_type'/ {print $1}' <<< "$disk_list" | paste -sd "," -)
+
+  echo "$matching_lines"
+}
+
+# # Sample input data
+# disk_list="Disk.Bay.0:Enclosure.Internal.0-1:RAID.Slot.1-1 Online Ok SSD
+# Disk.Bay.1:Enclosure.Internal.0-1:RAID.Slot.1-1 Online Ok SSD
+# Disk.Bay.2:Enclosure.Internal.0-1:RAID.Slot.1-1 Ready Ok HDD
+# Disk.Bay.9:Enclosure.Internal.0-1:RAID.Slot.1-1 Ready Ok HDD"
+disk_list=$(get_pdisks_with_attributes ${IDRAC_HOST})
 
 function get_all_vdisks() {
     python DeleteVirtualDiskREDFISH.py -ip ${IDRAC_HOST} -u "${IDRAC_USER}" -p "${IDRAC_PASS}" --get-virtualdisks ${RAID_CONTROLLER} | awk -F, '/Disk.Virtual/ {print $1}'
 }
+
 
 function delete_all_vdisks() {
 
@@ -29,22 +87,38 @@ function set_boot_vdisk() {
     python SetBootVdREDFISH.py -ip ${IDRAC_HOST} -u "${IDRAC_USER}" -p "${IDRAC_PASS}" --set ${RAID_CONTROLLER} --boot-vd "${1:?Missing boot vdisk}"
 }
 
-get_all_vdisks
-delete_all_vdisks
+function reset_controller() {
+    python ResetConfigStorageREDFISH.py -ip ${IDRAC_HOST} -u "${IDRAC_USER}" -p "${IDRAC_PASS}" --reset-controller ${RAID_CONTROLLER}
+}
 
-# Create a RAID 0 virtual disk for the OS
+echo "List all virtual disks"
+get_all_vdisks
+
+# echo "Delete all virtual disks"
+#delete_all_vdisks
+
+# Reset controller will wipe all vdisk, take ~3min
+reset_controller
+
+# Create a RAID 0 virtual disk for the OS using the SSDs
 python CreateVirtualDiskREDFISH.py -ip ${IDRAC_HOST} -u "${IDRAC_USER}" -p "${IDRAC_PASS}" \
     --create ${RAID_CONTROLLER} \
     --raid-level 0 \
-    --disks  Disk.Bay.8:${PV_SUFFIX},Disk.Bay.9:${PV_SUFFIX} \
+    --disks $(filter_disks "SSD") \
     --name OS
 
+# alternative way to generate the disk list
+# generate_pdisk_list_from_index 0 1
+# generate_pdisk_list_from_index {2..9}
+
 # Set the OS virtual disk as the boot disk
-set_boot_vdisk "$(get_all_vdisks)"
+set_boot_vdisk "$(get_all_vdisks | head -1)"
 
 # Create a RAID 50 virtual disk for the storage
 python CreateVirtualDiskREDFISH.py -ip ${IDRAC_HOST} -u "${IDRAC_USER}" -p "${IDRAC_PASS}" \
     --create ${RAID_CONTROLLER} \
     --raid-level 50 \
-    --disks  Disk.Bay.0:${PV_SUFFIX},Disk.Bay.1:${PV_SUFFIX},Disk.Bay.2:${PV_SUFFIX},Disk.Bay.3:${PV_SUFFIX},Disk.Bay.4:${PV_SUFFIX},Disk.Bay.5:${PV_SUFFIX},Disk.Bay.6:${PV_SUFFIX},Disk.Bay.7:${PV_SUFFIX} \
+    --disks $(filter_disks "HDD") \
     --name STORAGE
+
+get_all_vdisks
